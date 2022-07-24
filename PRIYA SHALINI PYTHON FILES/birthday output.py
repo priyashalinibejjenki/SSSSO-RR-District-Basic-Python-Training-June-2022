1eca13c03d86bd53bcdbb161e25d6490bfda3251
priyashalinibejjenki <108001271+priyashalinibejjenki@users.noreply.github.com>
2022-07-24 15:49:52 +0530

    PRIYA SHALINI PYTHON FILES

---
+++ PRIYA SHALINI PYTHON FILES/birthday output.py
@@ -0,0 +1,8 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================================================================== RESTART: E:/Adithi python files/birthday.py.py ====================================================================
+countdown for the birthday is: 
+121 days, 21 hours, 52 minutes, 33 seconds
+
+2022-07-24 03:07:26.172414
